3599db6c11b6a894f7a8c276dac79cea9fff204b
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2025-01-11 21:32:59 +0700

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AlsoKnownAsZira/basic-menggolang
+
+go 1.23.4
---
+++ tutorial_1/main.go
@@ -0,0 +1,6 @@
+package main 
+import "fmt" // fmt is a package that contains functions for printing formatted output and scanning input
+
+func main() {
+	fmt.Println("Hello, World!")
+}
